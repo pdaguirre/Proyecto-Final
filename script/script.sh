a53d1d571a1247eca167a55f4da44db857b34b68
Paula Doménica Aguirre <domeaguitorres@gmail.com>
2024-06-28 09:41:02 -0500

    Add files via upload

--- script/script.sh
+++ script/script.sh
@@ -6,6 +6,10 @@ if [ "$#" -eq 0 ]; then
   exit 1
 fi
 
+# Crear la carpeta results si no existe
+results_dir="results"
+mkdir -p "$results_dir"
+
 # Procesar cada archivo fasta proporcionado como argumento
 for file in "$@"; do
   # Verificar si el archivo existe
@@ -28,11 +32,10 @@ for file in "$@"; do
   # Verificar si el alineamiento contiene el outgroup especificado
   if grep -q "AY843739_Pseudis_minuta_" "aln_${file##*/}"; then
     # Comando para el cronograma con outgroup
-./iqtree2 -s "aln_${file##*/}" -m GTR+R+I -bb 1000 -alrt 1000 -safe -o AY843739_Pseudis_minuta_ --date-tip 0 -safe -nt auto --date-ci 100 --date-root -37.8
-
+    ./iqtree2 -s "aln_${file##/}" -m GTR+R+I -bb 1000 -alrt 1000 -safe -o AY843739_Pseudis_minuta_ --date-tip 0 -safe -nt auto --date-ci 100 --date-root -37.8 -pre "result_${file##/}"
   else
     # Comando para el cronograma sin outgroup
-    ./iqtree2 -s "aln_${file##*/}" -m GTR+R+I -bb 1000 -alrt 1000 -safe
+    ./iqtree2 -s "aln_${file##/}" -m GTR+R+I -bb 1000 -alrt 1000 -safe -pre "result_${file##/}"
   fi
 
   # Verificar si iqtree2 se ejecutó correctamente
@@ -40,6 +43,9 @@ for file in "$@"; do
     echo "Error: iqtree2 falló para el archivo $file."
     continue
   fi
+
+  # Mover los archivos resultantes a la carpeta results
+  mv result_${file##/}. "$results_dir/"
 
   echo "Proceso completado exitosamente para el archivo: $file."
 done
